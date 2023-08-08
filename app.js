e00bb5ffd7cf77c30d71e68f4e2ef91207a200a8
Jerry-Chang975 <jerry975607@gmail.com>
2023-08-08 20:24:43 +0800

    initialize handlebars layout

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
-
-
 const port = 3000;
+
+const { engine } = require('express-handlebars');
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
 
 // import routes
 const todosRoute = require('./router/routes');
 app.use('/todos', todosRoute);
 
+app.get('/', (req, res) => res.render('index'));
 
 app.listen(port, () =>
   console.log(`Server is running on http://localhost:${port}`)
